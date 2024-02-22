e6ecc9a98587bb92bdddd5cb593fbcc78a2c9e17
M1ck3y-ru <120647015+M1ck3y-ru@users.noreply.github.com>
2024-02-22 16:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 #CLEMENT GARCIA
 
 import time
-import motor
 import engine
 from machine import Pin, PWM
-import machine
-print("test")
-engine.setup()
-engine.loop()
+
+    
+while True:
+    engine.avancer(700) # allow you to move forward
+    time.sleep_ms(500) #keep executing the command for 5 ms
+    engine.tourner_droite(700) #allow you to turn right 
+    time.sleep_ms(500) #keep executing the command for 5 ms
+    engine.reculer(700) #allow you to turn backward 
+    time.sleep_ms(700) #keep executing the command for 5 ms
+    engine.arreter() #allow you to stop 
+    time.sleep_ms(700) #keep executing the command for 5 ms
+  
+    
+    
